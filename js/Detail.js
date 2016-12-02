2fe1d5525c61ad0fd5e40046eaf5b5df50ea91eb
eric12342006 <a0321050@gmail.com>
2016-12-02 18:19:47 +0800

    開左JSON試用

---
+++ js/Detail.js
@@ -0,0 +1,9 @@
+/**
+ * Created by a0321 on 2/12/2016.
+ */
+
+User1 = '[{"ac" : "abc", "pwd" : "123"}]';
+
+JData = [{ name: "Wing", age: "20", height: "182" },
+         { name: "Wind", age: "18", height: "165" },
+         { name: "Edge", age: "25", height: "171" }]
