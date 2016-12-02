72f9b2464694a5727aa3faca07b91756d38639d3
eric12342006 <a0321050@gmail.com>
2016-12-03 01:48:17 +0800

    Can Get the book in itemdetail through the item page

--- js/jquery.js
+++ js/jquery.js
@@ -11,13 +11,21 @@ $(document).ready(function(){
         $("#img6").attr("src","Novels/bk6.jpg");
         $("#img7").attr("src","Novels/bk7.jpg");
         $("#img8").attr("src","Novels/bk8.jpg");
-        $("#bk1").attr("href","detail.html?bk=novel1");
+
+        $("#bk1").attr("href","detail.html?bk=Novels/bk1.jpg");
+        $("#bk2").attr("href","detail.html?bk=Novels/bk2.jpg");
+        $("#bk3").attr("href","detail.html?bk=Novels/bk3.jpg");
+        $("#bk4").attr("href","detail.html?bk=Novels/bk4.jpg");
+        $("#bk5").attr("href","detail.html?bk=Novels/bk5.jpg");
+        $("#bk6").attr("href","detail.html?bk=Novels/bk6.jpg");
+        $("#bk7").attr("href","detail.html?bk=Novels/bk7.jpg");
+        $("#bk8").attr("href","detail.html?bk=Novels/bk8.jpg");
     });
 });
 
 $(document).ready(function(){
     $(".catbtn1").click(function(){
-        $("#img1").attr("src","textbook/textbk1.jpg");
+        $("#img1").attr("src","textbook/textbook1.jpg");
         $("#img2").attr("src","textbook/textbook2.jpg");
         $("#img3").attr("src","textbook/textbook3.jpg");
         $("#img4").attr("src","textbook/textbook4.jpg");
@@ -25,6 +33,15 @@ $(document).ready(function(){
         $("#img6").attr("src","textbook/textbook6.jpg");
         $("#img7").attr("src","textbook/textbook7.jpg");
         $("#img8").attr("src","textbook/textbook8.jpg");
+
+        $("#bk1").attr("href","detail.html?bk=textbook/textbook1.jpg");
+        $("#bk2").attr("href","detail.html?bk=textbook/textbook2.jpg");
+        $("#bk3").attr("href","detail.html?bk=textbook/textbook3.jpg");
+        $("#bk4").attr("href","detail.html?bk=textbook/textbook4.jpg");
+        $("#bk5").attr("href","detail.html?bk=textbook/textbook5.jpg");
+        $("#bk6").attr("href","detail.html?bk=textbook/textbook6.jpg");
+        $("#bk7").attr("href","detail.html?bk=textbook/textbook7.jpg");
+        $("#bk8").attr("href","detail.html?bk=textbook/textbook8.jpg");
     });
 });
 
@@ -38,5 +55,16 @@ $(document).ready(function(){
         $("#img6").attr("src","Travels/bk6.jpg");
         $("#img7").attr("src","Travels/bk7.jpg");
         $("#img8").attr("src","Travels/bk8.jpg");
+
+        $("#bk1").attr("href","detail.html?bk=Travels/bk1.jpg");
+        $("#bk2").attr("href","detail.html?bk=Travels/bk2.jpg");
+        $("#bk3").attr("href","detail.html?bk=Travels/bk3.jpg");
+        $("#bk4").attr("href","detail.html?bk=Travels/bk4.jpg");
+        $("#bk5").attr("href","detail.html?bk=Travels/bk5.jpg");
+        $("#bk6").attr("href","detail.html?bk=Travels/bk6.jpg");
+        $("#bk7").attr("href","detail.html?bk=Travels/bk7.jpg");
+        $("#bk8").attr("href","detail.html?bk=Travels/bk8.jpg");
     });
 });
+
+
