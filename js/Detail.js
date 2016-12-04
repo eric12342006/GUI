454828c0b1fef1f81f3cf9b73c8fbd5176982841
eric12342006 <a0321050@gmail.com>
2016-12-04 20:06:22 +0800

    Finally search works

--- js/Detail.js
+++ js/Detail.js
@@ -6,26 +6,26 @@ User1 = '[{"ac" : "abc", "pwd" : "123"}]';
 
 txt = '{"BookDes":[' +
     '{"BkType":"TB1","Bookname":"iOS SDK Programming","Editor":"James Brannan, Blake Ward","Price":"304","src":"textbook/textbook1.jpg" },' +
-    '{"BkType":"TB2","Bookname":"Programming in objective-c 2.0","Editor":"Gary Rosenzweig","Price":"400" },' +
-    '{"BkType":"TB3","Bookname":"IELTS 10","Editor":"Cambridge English","Price":"209" },' +
-    '{"BkType":"TB4","Bookname":"Learning iPad Programming","Editor":"Kirby Turner","Price":"320" },' +
-    '{"BkType":"TB5","Bookname":"SDK Programming For Dummies","Editor":"Rajiv Ramnath","Price":"285" },' +
-    '{"BkType":"TB6","Bookname":"Linux Network programming","Editor":"Jasmin Blanchette, Mark Summerfield","Price":"420" },' +
-    '{"BkType":"TB7","Bookname":"The Developers Guide to Social Programming","Editor":"Mark Hawker","Price":"310" },' +
-    '{"BkType":"TB8","Bookname":"A Complete Introduction to the Python Language","Editor":"Mark Summerfield","Price":"315" },'+
-    '{"BkType":"N1","Bookname":"Mercy of a Rude Stream: The Complete Novels","Editor":"Henry Roth, Joshua Ferris","Price":"380" },'+
-    '{"BkType":"N2","Bookname":"MSherlock Holmes The Complete Novels andStories Vol 2","Editor":"Arthur Conan Doyle","Price":"66" },'+
-    '{"BkType":"N3","Bookname":"Wind and Pinball Two Novels","Editor":"Haruki Murakami","Price":"247" },'+
-    '{"BkType":"N4","Bookname":"The Modern Library The 200 Best Novels in English Since 1950","Editor":"Carmen Callil","Price":"166" },'+
-    '{"BkType":"N5","Bookname":"A Jane Austen Education: How Six Novels Taught Me About Love","Editor":"William Deresiewicz","Price":"247" },'+
-    '{"BkType":"N6","Bookname":"The Wee Free Men","Editor":"Terry Pratchett","Price":"105" },'+
-    '{"BkType":"N7","Bookname":"Ben 10 Double Novels","Editor":"Parragon Book Service Ltd","Price":"70" },'+
-    '{"BkType":"N8","Bookname":"Rabbit Novels Vol.2","Editor":"John Updike","Price":"180" },'+
-    '{"BkType":"Tr1","Bookname":"NewZealand","Editor":"John Maker","Price":"200" },'+
-    '{"BkType":"Tr2","Bookname":"Bangkok","Editor":"John Maker","Price":"230" },'+
-    '{"BkType":"Tr3","Bookname":"Tailand","Editor":"John Maker","Price":"240" },'+
-    '{"BkType":"Tr4","Bookname":"New York City","Editor":"John Maker","Price":"250" },'+
-    '{"BkType":"Tr5","Bookname":"China","Editor":"John Maker","Price":"260" },'+
-    '{"BkType":"Tr6","Bookname":"Germany","Editor":"John Maker","Price":"270" },'+
-    '{"BkType":"Tr7","Bookname":"Turkey","Editor":"John Maker","Price":"280" },'+
-    '{"BkType":"Tr8","Bookname":"France","Editor":"Mark Summerfield","Price":"315" }]}';
+    '{"BkType":"TB2","Bookname":"Programming in objective-c 2.0","Editor":"Gary Rosenzweig","Price":"400","src":"textbook/textbook2.jpg" },' +
+    '{"BkType":"TB3","Bookname":"IELTS 10","Editor":"Cambridge English","Price":"209","src":"textbook/textbook3.jpg" },' +
+    '{"BkType":"TB4","Bookname":"Learning iPad Programming","Editor":"Kirby Turner","Price":"320","src":"textbook/textbook4.jpg" },' +
+    '{"BkType":"TB5","Bookname":"SDK Programming For Dummies","Editor":"Rajiv Ramnath","Price":"285","src":"textbook/textbook5.jpg" },' +
+    '{"BkType":"TB6","Bookname":"Linux Network programming","Editor":"Jasmin Blanchette, Mark Summerfield","Price":"420","src":"textbook/textbook6.jpg" },' +
+    '{"BkType":"TB7","Bookname":"The Developers Guide to Social Programming","Editor":"Mark Hawker","Price":"310","src":"textbook/textbook7.jpg" },' +
+    '{"BkType":"TB8","Bookname":"A Complete Introduction to the Python Language","Editor":"Mark Summerfield","Price":"315","src":"textbook/textbook8.jpg" },'+
+    '{"BkType":"N1","Bookname":"Mercy of a Rude Stream: The Complete Novels","Editor":"Henry Roth, Joshua Ferris","Price":"380","src":"Novels/bk1.jpg" },'+
+    '{"BkType":"N2","Bookname":"MSherlock Holmes The Complete Novels andStories Vol 2","Editor":"Arthur Conan Doyle","Price":"66","src":"Novels/bk2.jpg" },'+
+    '{"BkType":"N3","Bookname":"Wind and Pinball Two Novels","Editor":"Haruki Murakami","Price":"247","src":"Novels/bk3.jpg" },'+
+    '{"BkType":"N4","Bookname":"The Modern Library The 200 Best Novels in English Since 1950","Editor":"Carmen Callil","Price":"166","src":"Novels/bk4.jpg" },'+
+    '{"BkType":"N5","Bookname":"A Jane Austen Education: How Six Novels Taught Me About Love","Editor":"William Deresiewicz","Price":"247","src":"Novels/bk5.jpg" },'+
+    '{"BkType":"N6","Bookname":"The Wee Free Men","Editor":"Terry Pratchett","Price":"105","src":"Novels/bk6.jpg" },'+
+    '{"BkType":"N7","Bookname":"Ben 10 Double Novels","Editor":"Parragon Book Service Ltd","Price":"70","src":"Novels/bk7.jpg" },'+
+    '{"BkType":"N8","Bookname":"Rabbit Novels Vol.2","Editor":"John Updike","Price":"180","src":"Novels/bk8.jpg" },'+
+    '{"BkType":"Tr1","Bookname":"NewZealand","Editor":"John Maker","Price":"200","src":"Travels/bk1.jpg" },'+
+    '{"BkType":"Tr2","Bookname":"Bangkok","Editor":"John Maker","Price":"230","src":"Travels/bk2.jpg" },'+
+    '{"BkType":"Tr3","Bookname":"Tailand","Editor":"John Maker","Price":"240","src":"Travels/bk3.jpg" },'+
+    '{"BkType":"Tr4","Bookname":"New York City","Editor":"John Maker","Price":"250","src":"Travels/bk4.jpg" },'+
+    '{"BkType":"Tr5","Bookname":"China","Editor":"John Maker","Price":"260","src":"Travels/bk5.jpg" },'+
+    '{"BkType":"Tr6","Bookname":"Germany","Editor":"John Maker","Price":"270","src":"Travels/bk6.jpg" },'+
+    '{"BkType":"Tr7","Bookname":"Turkey","Editor":"John Maker","Price":"280","src":"Travels/bk7.jpg" },'+
+    '{"BkType":"Tr8","Bookname":"France","Editor":"Mark Summerfield","Price":"315","src":"Travels/bk8.jpg" }]}';
