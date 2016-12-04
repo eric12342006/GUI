3171b87a8fcba6b968dc5db4600df1d9a5cbdaab
cheukwang2 <cheukwang222@gmail.com>
2016-12-04 23:23:22 +0800

    update

--- js/jquery.js
+++ js/jquery.js
@@ -68,11 +68,18 @@ $(document).ready(function(){
 });
 
 
+function getCurrentDay(){
+    var currentdate = new Date();
+    return currentdate.toDateString();
+}
+
+
 
 /*********************************/
 var cart = [];
 var duplicate = false;
 var tempName;
+var totalCost = 0;
 
 var Item = function(name, price, count){
     this.name = name;
@@ -109,6 +116,7 @@ function saveCart(){
     localStorage.setItem("shoppingCart",JSON.stringify(cart));
 }
 
+
 function removeItemFromCart(name){ //Remove one item
     for (var i in cart){
         if(cart[i].name === name){
@@ -135,43 +143,22 @@ function removeAllItemFromCart(name){ // removes all item with name
     location.reload();
 }
 
-function listCart(){
-    var cartCopy = [];
-    for(var i in cart){
-        var item = cart[i];
-        var itemCopy = {};
-        for(var p in item){
-            itemCopy[p] = item[p];
-        }
-        cartCopy.push(itemCopy);
-    }
-    return cartCopy;
-    console.log(cartCopy);
-}
-
-$(".add-to-cart").click(function(event){
-    event.preventDefault();
-    var name = $(this).attr();
-    var price = Number($(this).attr());
-
-    addItemToCart(name,price,1);
-});
-
 function displayCart(){
     var cartArray =  JSON.parse(localStorage.getItem("shoppingCart"));
     var totalPrice = JSON.parse(localStorage.getItem("totalPrice"));
     var output = "";
+    var testTotalPrice = 0;
     for (var i in cartArray){
        tempName = cartArray[i].name;
-       console.log(tempName);
+       testTotalPrice += Number(cartArray[i].price) * Number(cartArray[i].count);
        output += "<li>"+cartArray[i].name+" "+cartArray[i].price+" "+cartArray[i].count+"<input type='button' value='DELETE ONE' onclick='removeItemFromCart(tempName)'>"+"<input type='button' value='DELETE ALL' onclick='removeAllItemFromCart(tempName)'>"+"</li>";
     }
     $("#show-cart").html(output);
-    $("#total-cart").html(totalPrice);
+    $("#total-cart").html(testTotalPrice);
 }
 
 function totalCart(){
-    var totalCost = 0;
+    totalCost = 0;
     for (var i in cart){
             totalCost += cart[i].price * cart[i].count;
     }
@@ -180,13 +167,44 @@ function totalCart(){
     return totalCost;
 }
 
-function getCart(){
-    var cartArray =  JSON.parse(localStorage.getItem("shoppingCart"));
-    return cartArray();
-}
-
 function loadCart(){
     cart = JSON.parse(localStorage.getItem("shoppingCart"));
     console.log(cart);
+    totalCart();
+}
+
+function saveAsOrder(){
+    //var check = localStorage.getItem('Order');
+   // if (typeof check !== 'undefined' && check !== null){
+    //}
+   // var abc = [];
+    //localStorage.setItem("shoppingCart",JSON.stringify(abc));
+
+    //var cartArray =  JSON.parse(localStorage.getItem("shoppingCart"));
+   // console.log(cartArray);
+
+    var cartArray =  JSON.parse(localStorage.getItem("shoppingCart"));
+    localStorage.setItem("temp",JSON.stringify(cartArray));
+    var check = localStorage.getItem('temp');
+    if (typeof check !== 'undefined' && check !== null){
+            var orderArray = JSON.parse(localStorage.getItem("order"));
+            localStorage.setItem("order",JSON.stringify(cartArray));
+        }
+    var abc = [];
+    localStorage.setItem("shoppingCart",JSON.stringify(abc));
+}
+
+function displayOrder(){
+    var orderArray =  JSON.parse(localStorage.getItem("order"));
+    console.log(orderArray);
+    var totalPrice = JSON.parse(localStorage.getItem("totalPrice"));
+    var output = "";
+    for (var i in orderArray){
+        tempName = orderArray[i].name;
+        console.log(tempName);
+        output += "<li>"+orderArray[i].name+" "+orderArray[i].price+" "+orderArray[i].count+" "+getCurrentDay()+"</li>";
+    }
+    $("#show-cart").html(output);
+    $("#total-cart").html(totalPrice);
 }
 
